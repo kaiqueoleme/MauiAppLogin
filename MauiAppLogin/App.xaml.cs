7b9b65b9f91502b445eda2b5beb15e96d53eaf6a
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2024-10-22 01:32:23 -0300

    Finalização do Projeto Login

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -6,7 +6,8 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new Login();
+
         }
         protected override Window CreateWindow(IActivationState activationState)
         {
--- MauiAppLogin/Login.xaml.cs
+++ MauiAppLogin/Login.xaml.cs
@@ -2,8 +2,50 @@ namespace MauiAppLogin;
 
 public partial class Login : ContentPage
 {
-	public Login()
-	{
-		InitializeComponent();
-	}
+    public Login()
+    {
+        InitializeComponent();
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            List<DadosUsuario> lista_usuarios =
+            [
+                new DadosUsuario()
+                {
+                    Usuario = "jose",
+                    Senha = "123"
+                }, 
+                new DadosUsuario()
+                {
+                    Usuario = "maria",
+                    Senha = "321"
+                }
+            ];
+
+            DadosUsuario dados_digitados = new()
+            {
+                Usuario = txt_usuario.Text,
+                Senha = txt_senha.Text
+            };
+
+            // LINQ
+            if (lista_usuarios.Any(i => (dados_digitados.Usuario == i.Usuario && dados_digitados.Senha == i.Senha) ))
+            {
+                await SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+
+                App.Current.MainPage = new Protegida();
+            } else
+            {
+                throw new Exception("Usuário ou senha incorretos.");
+            }
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Fechar");
+        }
+    }
 }
